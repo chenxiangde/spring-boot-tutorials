219ca94b64ecdeeaa5d85741fac3d939859c9aef
chenxiangde <chenxiangde@ppdai.com>
2018-05-02 13:20:46 +0800

    Swagger修改提交

--- spring-boot-swagger2/src/main/java/springboot/swagger/config/Swagger2Config.java
+++ spring-boot-swagger2/src/main/java/springboot/swagger/config/Swagger2Config.java
@@ -26,7 +26,7 @@ public class Swagger2Config {
                 .apiInfo(apiInfo())
                 .select()
                 //为当前包路径
-                .apis(RequestHandlerSelectors.basePackage("com.bytebeats.springboot.ch2.controller"))
+                .apis(RequestHandlerSelectors.basePackage("springboot.swagger.controller"))
                 .paths(PathSelectors.any())
                 .build();
     }
